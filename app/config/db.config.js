8becd807af3f67e2de2bb15ec97a2c6c9fc1a8d6
Arash Mohammadi <arash13131313@gmail.com>
2021-08-28 12:52:44 +0430

    change database to server

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "",
-    DB: "busdb",
-    dialect: "mysql",
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
+  HOST: "87.98.250.180",
+  USER: "mysetad1_arash",
+  PASSWORD: "3eh0V+NOx-g}",
+  DB: "mysetad1_busdb",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
 };
